2aa590bb33639a870bd900aed558be299af8b75b
Philip Leal <philleal@gmail.com>
2026-08-22 21:29:07 -0500

    Fixing the cameras

--- src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -293,7 +293,7 @@ public class Vision
      {
        Desktop.getDesktop().browse(new URI("http://localhost:1182/"));
        Desktop.getDesktop().browse(new URI("http://localhost:1184/"));
-       Desktop.getDesktop().browse(new URI("http://localhost:1186/"));
+       //Desktop.getDesktop().browse(new URI("http://localhost:1186/"));
      } catch (IOException | URISyntaxException e)
      {
        e.printStackTrace();
@@ -341,30 +341,30 @@ public class Vision
     /**
      * Left Camera
      */
-    LEFT_CAM("left",
-             new Rotation3d(0, Math.toRadians(-24.094), Math.toRadians(30)),
-             new Translation3d(Units.inchesToMeters(12.056),
-                               Units.inchesToMeters(10.981),
-                               Units.inchesToMeters(8.44)),
+    FRONT_CAM("front",
+             new Rotation3d(0, Math.toRadians(0.0), Math.toRadians(180.0)),
+             new Translation3d(Units.inchesToMeters(0.0),
+                               Units.inchesToMeters(0.0),
+                               Units.inchesToMeters(0.0)),
              VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1)),
     /**
      * Right Camera
      */
-    RIGHT_CAM("right",
-              new Rotation3d(0, Math.toRadians(-24.094), Math.toRadians(-30)),
-              new Translation3d(Units.inchesToMeters(12.056),
-                                Units.inchesToMeters(-10.981),
-                                Units.inchesToMeters(8.44)),
-              VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1)),
+    BACK_CAM("back",
+              new Rotation3d(0, Math.toRadians(0.0), Math.toRadians(0.0)),
+              new Translation3d(Units.inchesToMeters(0.0),
+                                Units.inchesToMeters(0.0),
+                                Units.inchesToMeters(0.0)),
+              VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1));
     /**
      * Center Camera
      */
-    CENTER_CAM("center",
-               new Rotation3d(0, Units.degreesToRadians(18), 0),
-               new Translation3d(Units.inchesToMeters(-4.628),
-                                 Units.inchesToMeters(-10.687),
-                                 Units.inchesToMeters(16.129)),
-               VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1));
+    // CENTER_CAM("center",
+    //            new Rotation3d(0, Units.degreesToRadians(18), 0),
+    //            new Translation3d(Units.inchesToMeters(-4.628),
+    //                              Units.inchesToMeters(-10.687),
+    //                              Units.inchesToMeters(16.129)),
+    //            VecBuilder.fill(4, 4, 8), VecBuilder.fill(0.5, 0.5, 1));
 
     /**
      * Latency alert to use when high latency is detected.
